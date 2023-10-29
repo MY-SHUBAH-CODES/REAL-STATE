37870a42252d42337a331425f6dbcbd8013dbc0d
shubh730 <shubhi231305@gmail.com>
2023-10-29 22:50:46 +0530

    ifuhiwefi

--- REAL_STATE/settings.py
+++ REAL_STATE/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-l$*dgeyah@0)j*8@xf+zwaj%_wy#@lm@wf(-g*ad6e#ccfu36v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
